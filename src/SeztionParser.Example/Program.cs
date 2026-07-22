10cc20e6f68a2bcc1231ceb367e7d1dddc51156c
MrDave1999 <daveseva2010@hotmail.es>
2026-07-22 08:22:09 -0500

    refactor: improve example project

--- src/SeztionParser.Example/Program.cs
+++ src/SeztionParser.Example/Program.cs
@@ -2,19 +2,43 @@
 
 ISectionsData sections = SectionsFile.Load("Aim_Headshot.ini");
 
-Console.WriteLine("[Alpha]");
-ISectionData alphaSection = sections["Alpha"];
-foreach (string data in alphaSection)
+Console.WriteLine("[AlphaTeamLocations]");
+ISectionData alphaTeamLocations = sections["AlphaTeamLocations"];
+foreach (string data in alphaTeamLocations)
     Console.WriteLine(data);
 
 Console.WriteLine();
 
-Console.WriteLine("[Beta]");
-ISectionData betaSection = sections["Beta"];
-foreach (string data in betaSection)
+Console.WriteLine("[BetaTeamLocations]");
+ISectionData betaTeamLocations = sections["BetaTeamLocations"];
+foreach (string data in betaTeamLocations)
+    Console.WriteLine(data);
+
+Console.WriteLine();
+
+Console.WriteLine("[RedFlagLocation]");
+ISectionData redFlagLocation = sections["RedFlagLocation"];
+foreach (string data in redFlagLocation)
+    Console.WriteLine(data);
+
+Console.WriteLine();
+
+Console.WriteLine("[BlueFlagLocation]");
+ISectionData blueFlagLocation = sections["BlueFlagLocation"];
+foreach (string data in blueFlagLocation)
     Console.WriteLine(data);
 
 int interior = sections.GetFirstLineInt("Interior");
 Console.WriteLine();
 Console.WriteLine("[Interior]");
 Console.WriteLine(interior);
+
+int weather = sections.GetFirstLineInt("Weather");
+Console.WriteLine();
+Console.WriteLine("[Weather]");
+Console.WriteLine(weather);
+
+int worldTime = sections.GetFirstLineInt("WorldTime");
+Console.WriteLine();
+Console.WriteLine("[WorldTime]");
+Console.WriteLine(worldTime);
